5371a399e2b36ff5d4f24adfc8543618fc604afe
Thiago Romero <thiago.romero.ext@dasa.com.br>
2020-11-24 20:48:39 -0300

    completing author'

--- kottestcdyn/demo/src/main/kotlin/com/example/demo/model/Author.kt
+++ kottestcdyn/demo/src/main/kotlin/com/example/demo/model/Author.kt
@@ -1,12 +1,19 @@
 package com.example.demo.model
 
-data class Author (private val id: String,
-                    private val name:String,
-                private val documentType:DocumentType,
-            private val age:Int,
-        private val email:String)
+data class Author(private val id: String,
+                  private val name: String,
+                  private val documentType: DocumentType,
+                  private val age: Int,
+                  private val address:Address,
+                  private val email: String)
 
 
-enum class DocumentType(
-     private val CPF:String
+enum class DocumentType {
+    CPF,
+    RG
+}
+
+data class Address(
+        private val street:String,
+        private val number:Int
 )
